e1d1233bd64e6fab8ae2a0e6eb20e128fa63adfd
tatarudesxbeats <72546759+tatarudesxbeats@users.noreply.github.com>
2020-10-08 19:08:24 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,70 @@
+$(document).ready(function () {
+  $(".carousel__inner").slick({
+    speed: 1200,
+    adaptiveHeight: true,
+    prevArrow:
+      '<button type="button" class="prev"><img src="../icons/left.png"></button>',
+    nextArrow:
+      '<button type="button" class="next"><img src="../icons/right.png"></button>',
+    responive: [
+      {
+        breakpoint: 992,
+        settings: {
+          dots: true,
+          arrows: false,
+        },
+      },
+    ],
+    responsive: [
+      {
+        breakpoint: 575,
+        settings: {
+          width: 50,
+          height: 50,
+        },
+      },
+    ],
+  });
+
+  $("ul.catalog__tabs").on(
+    "click",
+    "li:not(.catalog__tab_active)",
+    function () {
+      $(this)
+        .addClass("catalog__tab_active")
+        .siblings()
+        .removeClass("catalog__tab_active")
+        .closest("div.container")
+        .find("div.catalog__content")
+        .removeClass("catalog__content_active")
+        .eq($(this).index())
+        .addClass("catalog__content_active");
+    }
+  );
+
+  function toggleSlide(item) {
+    $(item).each(function (i) {
+      $(this).on("click", function (e) {
+        e.preventDefault();
+        $(".catalog-item__content")
+          .eq(i)
+          .toggleClass("catalog-item__content_active");
+        $(".catalog-item__list").eq(i).toggleClass("catalog-item__list_active");
+      });
+    });
+  }
+  toggleSlide(".catalog-item__link");
+  toggleSlide(".catalog-item__back");
+
+  // Modal
+
+  $("[data-modal=consultation]").on("click", function () {
+    $(".overlay, #consultation").fadeIn("slow");
+  });
+  $(".modal__close").on("click", function () {
+    $(".overlay, #consultation, #thanks, #order").fadeOut("slow");
+  });
+  $(".catalog-item__btn").on("click", function () {
+    $(".overlay, #order").fadeIn("slow");
+  });
+});
